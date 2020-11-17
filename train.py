169cf4e284f950926d0458b818e85b863a74e003
UESTC-Liuxin <625092890@qq.com>
2020-11-17 15:24:29 +0800

    add 计算数据集的类分布代码

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from metrics.metrics import runningScore, averageMeter
 from tqdm import tqdm
+from torchviz import make_dot
 
 class Trainer(object):
 
@@ -92,6 +93,9 @@ class Trainer(object):
             # start_time = time.time()
             batch=self.dict_to_cuda(batch)
             output=self.model(batch)
+            #print(output)
+            #g=make_dot(output['trunk_out'])
+            #g.render('danet',directory='/home/cyl/SkmtSeg',view=False)
             loss=self.criterion(output,batch).cuda()
             self.optimizer.zero_grad()
             loss.backward()
