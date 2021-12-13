d2c6b887caa2362b2eddf67b2e16b6d3ee8ff717
maglanyulan <1012568337@qq.com>
2021-12-13 10:24:31 +0800

    wceloss

--- criterion/WCELoss.py
+++ criterion/WCELoss.py
@@ -12,7 +12,9 @@ import torch.nn as nn
 
 class WCELoss(nn.Module):
 
-    def __init__(self,weight=None,*args,**kwargs):
+    def __init__(self,#weight=[447433227,30668326,863040,56023299,36496764,4911119,5293877,1005469,5017141,700838,14504891],
+                 weight=[0.22762148,0.22762148,0.91752577,0.22791293,0.22967742,1.11949686,0.99441341,0.72357724,0.71485944,1,0.71774194],
+                 *args,**kwargs):
         super(WCELoss,self).__init__()
         if(weight):
             weight=1-torch.Tensor(weight).cuda()
