0f78be33d04c4aa82b58bb67f9c5f6e4dd4863fe
maglanyulan <1012568337@qq.com>
2021-12-22 14:42:08 +0800

    yuchuli

--- tools/datasets_convert/chart.py
+++ tools/datasets_convert/chart.py
@@ -3,19 +3,49 @@ import numpy as np
 path = '../confusion_matrix.csv'
 data = pd.DataFrame(pd.read_csv(path))
 
-print(data.columns)
 z=0
+fenmu=np.zeros(11)
 for i in data.columns:
     z = z + 1
     if z==1:
         continue
     k=0
-    fenmu=np.sum(data[i].values[:11])
+    for j in data[i].values:
+        if k > 10:
+            continue
+        if str(j) == 'nan':
+            fenmu[k] +=0
+        else:
+            fenmu[k] +=j
+        k+=1
+z=0
+print(fenmu)
+for i in data.columns:
+    z = z + 1
+    if z==1:
+        continue
+    k=0
     for j in data[i].values:
         if k>10:
             continue
         if str(j) == 'nan':
-            print('[{},{},{}]'.format(i, k, 0), end=",")
+            print('[{},{},{}]'.format(k,i, 0), end=",")
         else:
-            print('[{},{},{:.2f}]'.format(k,i,int(j)/fenmu),end = ",")
+            print('[{},{},{:.2f}]'.format(k,i,int(j)/fenmu[k]),end = ",")
         k=k+1
+##列和
+# for i in data.columns:
+#     z = z + 1
+#     if z==1:
+#         continue
+#     k=0
+#     fenmu=np.sum(data[i].values[:11])
+#     print(fenmu)
+#     for j in data[i].values:
+#         if k>10:
+#             continue
+#         if str(j) == 'nan':
+#             print('[{},{},{}]'.format(i, k, 0), end=",")
+#         else:
+#             print('[{},{},{:.2f}]'.format(k,i,int(j)/fenmu),end = ",")
+#         k=k+1
