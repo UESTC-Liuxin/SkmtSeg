41472735c3d815c362f12689735193a369874f3c
maglanyulan <1012568337@qq.com>
2022-03-05 19:59:56 +0800

    修改测试

--- dataloader/skmt.py
+++ dataloader/skmt.py
@@ -118,8 +118,8 @@ class SkmtDataSet(Dataset):
         return _img, _target
 
     def transform_tr(self, sample):
-        augm = au.Augment()
-        sample = augm(sample)
+        # augm = au.Augment()
+        # sample = augm(sample)
         composed_transforms = transforms.Compose([
             # tr.RandomHorizontalFlip(),#随机水平翻转
             tr.RandomScaleCrop(base_size=self.args.image_size, crop_size=self.args.crop_size),#随机裁剪
